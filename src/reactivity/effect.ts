574ff5c2971587640b81ff5b007728aff05b5ba0
yang <17368465776@163.com>
2023-12-19 23:25:13 +0800

    feat 支持过期回调

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,4 +1,3 @@
-
 /*
  * @Author: yang 17368465776@163.com
  * @Date: 2023-07-30 10:09:33
@@ -8,8 +7,15 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export let activeEffect
+const effectStack = []
 const bucket  = new WeakMap()
 
+/**
+ * @description: 依赖追踪
+ * @param {*} target
+ * @param {*} key
+ * @return {*}
+ */
 function track(target, key) {
     if(!activeEffect) {
         return
@@ -23,25 +29,42 @@ function track(target, key) {
         depsMap.set(key, (deps = new Set()))
     }
     deps.add(activeEffect)
-    console.log('副作用函数 依赖收集', deps)
     activeEffect.deps.push(deps)
 }
 
+/**
+ * @description: 触发依赖重新执行
+ * @param {*} target
+ * @param {*} key
+ * @return {*}
+ */
 function trigger(target, key) {
     const depsMap = bucket.get(target)
     if(!depsMap) {
         return 
     }
     const effects = depsMap.get(key)
-    const effectToRun = new Set(effects)
-
+    const effectToRun = new Set()
+    effects && effects.forEach(effectFn => {
+        // trigger 触发执行的副作用函数与当前正在执行的副作用函数形同，则不出发执行
+        if(effectFn !== activeEffect) {
+        effectToRun.add(effectFn)
+        }
+    })
    effectToRun.forEach(effectFn => {
-    effectFn()
+    // 运行调度器
+    if(effectFn.options.scheduler) {
+        effectFn.options.scheduler(effectFn)
+    } else {
+        effectFn()
+    }
     })
 }
 
 const obj = {
-    name: 'simtek'
+    name: 'simtek',
+    foo: 1,
+    bar: 2
 }
 export const proxyObj = new Proxy(obj, {
     get(target, key) {
@@ -55,42 +78,156 @@ export const proxyObj = new Proxy(obj, {
     }
 
 })
-export function effect(fn) {
+export function effect(fn, options = {}) {
     const effectFn = () => {
-        activeEffect = effectFn
         cleanup(effectFn)
-        fn()
+        activeEffect = effectFn
+        effectStack.push(effectFn)
+        const res = fn()
+        effectStack.pop()
+        activeEffect = effectStack[effectStack.length  -1]
+        return res
     }
+    effectFn.options = options
     effectFn.deps = []
-    console.log('effectfn...', effectFn.deps)
-    effectFn()
+    if(!options.lazy) {
+        return effectFn()
+    }
+   return effectFn
 }
 
 function cleanup(effectFn) {
     for(let i = 0; i < effectFn.deps.length; i++) {
         const deps = effectFn.deps[i]
-        console.log('delete effectFn', deps)
         deps.delete(effectFn)
-        console.log('deleted... effectFn', deps)
     }
     effectFn.deps.length = 0
 }
+const jobQueue = new Set()
+const p = Promise.resolve()
+let isFlushing = false
+
+function flushJob() {
+    if(isFlushing) {
+        return 
+    }
+    isFlushing = true
+    p.then(() => {
+        console.log('flus json', jobQueue)
+        jobQueue.forEach(job => job())
+    }).finally(() => {
+        isFlushing = false
+    })
+
+}
 
 effect(() => {
-    console.log('effect run')
-    document.body.innerText = proxyObj.name ==='' ? 'tttt' : proxyObj.h
+    console.log(proxyObj.foo)
+}, {scheduler(fn) {
+    console.log('add fn to jobQueue')
+    jobQueue.add(fn)
+    flushJob()
+}})
+
+
+
+function computed(getter) {
+    let value
+    // 脏数据的标志值
+    let dirty = true
+    // 获取effectFn
+    const effectFn  = effect(getter, {lazy: true, scheduler() {
+        dirty = true
+        trigger(obj, 'value')
+    }})
+    const obj = {
+        get value() {
+            if(dirty) {
+                value = effectFn()
+                dirty = false               
+            }
+            track(target, 'value')
+            return value
+        }
+    }
+    return obj
+}
+
+const sumRes = computed(() => proxyObj.bar + proxyObj.foo)
+
+
+
+function watch(source, cb, options = {}) {
+    let getter
+    let newValue, oldValue
+    if(typeof source === 'function') {
+        getter = source
+    } else {
+        getter = () => traverse(source)
+    }
+    // 处理过期回调
+    let cleanup
+    function onInvalidate(fn) {
+        cleanup = fn
+    }
+    console.log('cleanup =====> ', cleanup)
+    const job = () => {
+        newValue = effectFn()
+        // 先执行过期回调
+        if(cleanup) {
+            cleanup()
+        }
+        cb(newValue, oldValue, onInvalidate)
+        oldValue = newValue
+    }
+    // lazy 返回effetFn, job 调度器
+    const effectFn = effect(getter, { lazy: true, scheduler: job})
+    if(options.immedinate) {
+        job()
+    } else {
+        // 初始化oldValue
+        oldValue = effectFn()
+    }
+   
+}
+
+function traverse(value, seen = new Set()) {
+    // 基础类型不处理
+    if(typeof value !== 'object' || typeof value == null || seen.has(value)) {
+        return 
+    }
+    seen.add(value)
+    for(const k in value) {
+        traverse(value[k], seen)
+    }
+    return value
+}
+
+let finalData
+
+watch(proxyObj, async (newValue, oldValue, onInvalidate) => {
+    // 过期标识
+    let expired = false
+    onInvalidate(() => 
+    {   
+        expired = true
+
+    })
+    const res = await new Promise((resolve, rejcet) => {
+        
+        if(newValue.bar === 3) {
+            setTimeout(() => resolve('first value'),  4 * 1000)
+            
+        } else {
+            resolve(value++)
+        }
+    })
+    console.log(`res is ${res}`)
+    if(!expired) {
+        console.log(`set Res ${res}`)
+        finalData = res
+    }
+
 })
-
-
-setTimeout(() => {
-    proxyObj.h = `1111`
-    console.log(bucket)
-}, 3000);
-
-effect(() => {
-    
-    proxyObj.name
-})
-
 
 
